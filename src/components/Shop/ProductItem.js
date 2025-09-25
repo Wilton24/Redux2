247c93a60aa8096b900a9402a1fe3bbb2e36e446
Wilton Batiquin <cbasher24@gmail.com>
2025-09-25 19:44:46 +0800

    Working cartStore. Minor "key" prop issue

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,27 @@
 import Card from '../UI/Card';
+import { useSelector } from 'react-redux';
 import classes from './ProductItem.module.css';
+import { useDispatch } from 'react-redux';
+import { addItem, removeItem } from "../../store/cartStore";
+
 
 const ProductItem = (props) => {
+  const dispatch = useDispatch();
   const { title, price, description } = props;
+
+  const cartItems = useSelector((state) => state.cart.items);
+
+  function addItemToCart() {
+    console.log('Cart Items:', cartItems);
+    dispatch(
+      addItem({
+        id: props.id,
+        title,
+        price,
+      })
+    );
+
+  }
 
   return (
     <li className={classes.item}>
@@ -13,7 +32,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addItemToCart}>Add to Cart</button>
         </div>
       </Card>
     </li>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-
 import './index.css';
 import App from './App';
-import store from './store/store.js';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
